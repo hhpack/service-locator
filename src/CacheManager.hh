ac3a61fcecbc493f7ee170c9697eddd86dc86a5d
holyshared <holy.shared.design@gmail.com>
2016-04-09 17:58:22 +0900

    Update signatures

--- src/CacheManager.hh
+++ src/CacheManager.hh
@@ -5,5 +5,6 @@ namespace hhpack\service;
 interface CacheManager
 {
     public function has<Tu as Service>(classname<Tu> $name) : bool;
-    public function lookup<Tu as Service>(classname<Tu> $name) : Tu;
+    public function get<Tu as Service>(classname<Tu> $name) : Tu;
+    public function set<Tu as Service>(classname<Tu> $name, Tu $service) : void;
 }
